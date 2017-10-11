ac9063675127c5089bf12eba3cc7c76f3a261796
Keigo Yamamoto <k5.trismegistus@gmail.com>
2017-10-12 00:39:39 +0900

    feat: Updated config about sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'videos#index'
 
   resources :videos, only: [:show]
   resources :taggings, only: [:create, :destroy]
+  mount Sidekiq::Web => '/sidekiq'
 end
