83bd9e2cca6fac23560493d8dce5703e28698544
keigo <k5.trismegistus@gmail.com>
2019-09-15 21:33:22 +0900

    :lipstick: Player view

--- app/app/controllers/videos_controller.rb
+++ app/app/controllers/videos_controller.rb
@@ -1,15 +1,15 @@
 class VideosController < ApplicationController
   def index
     unless params[:tag].present?
-      @videos = Video.order(:id).page(params[:page]).per(20)
+      @videos = Video.includes(taggings: :tag).order(:id).page(params[:page]).per(20)
     else
-      @tag = Tag.find_by(content: params[:tag])
+      @tag = Tag.includes(taggings: :video).find_by(content: params[:tag])
       @videos = @tag.videos.order(:id).page(params[:page]).per(20)
     end
   end
 
   def show
-    @video = Video.find(params[:id])
+    @video = Video.includes(taggings: :tag).find(params[:id])
   end
 
   def destroy
